62b194a1fd9d85a7298f05ce70195729ee1b83cf
shin4299 <shin4299@gmail.com>
2018-03-24 10:16:37 +0900

    Update xiaomi-weather.groovy

--- devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
+++ devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
@@ -1,5 +1,5 @@
 /**
- *  Xiaomi Weather (v.0.0.1)
+ *  Xiaomi Sensor Temperature & Humidity (v.0.0.1)
  *
  * MIT License
  *
@@ -26,67 +26,123 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
 */
- 
+
 import groovy.json.JsonSlurper
 
 metadata {
-	definition (name: "Xiaomi Weather", namespace: "fison67", author: "fison67") {
-        capability "Sensor"						
+	definition (name: "Xiaomi Sensor HT", namespace: "fison67", author: "fison67") {
+        capability "Temperature Measurement"
+        capability "Relative Humidity Measurement"
+        capability "Sensor"
+        capability "Battery"
          
-        attribute "temperature", "string"
-        attribute "humidity", "string"
-        attribute "airPressure", "string"
+        attribute "pressure", "string"
+	attribute "maxTemp", "number"
+	attribute "minTemp", "number"
+	attribute "maxHumidity", "number"
+	attribute "minHumidity", "number"
+	attribute "multiAttributesReport", "String"
         
         attribute "lastCheckin", "Date"
 	}
 
 
-	simulator {
+	simulator {}
+	preferences {
+		input name: "displayTempHighLow", type: "bool", title: "Display high/low temperature?"
+		input name: "displayHumidHighLow", type: "bool", title: "Display high/low humidity?"
 	}
 
+
 	tiles {
-		multiAttributeTile(name:"temperature", type: "generic", width: 6, height: 4, canChangeIcon: true){
-			tileAttribute ("device.temperature", key: "PRIMARY_CONTROL") {
-                attributeState "val", label:'${currentValue}', icon:"st.Weather.weather2", 
+        multiAttributeTile(name:"temperature", type:"generic", width:6, height:4) {
+            tileAttribute("device.temperature", key: "PRIMARY_CONTROL") {
+                attributeState("temperature", label:'${currentValue}',
                     backgroundColors:[
-                        [value: 10, color: "#fc97ab"],
-                        [value: 20, color: "#979afc"],
-                        [value: 30, color: "#565bf7"],
-                        [value: 40, color: "#edb1b7"],
-                        [value: 50, color: "#ed959e"],
-                        [value: 60, color: "#ed808b"],
-                        [value: 70, color: "#e25a68"],
-                        [value: 80, color: "#dd3042"]
+                        // Fahrenheit color set
+                        [value: 0, color: "#153591"],
+                        [value: 5, color: "#1e9cbb"],
+                        [value: 10, color: "#90d2a7"],
+                        [value: 15, color: "#44b621"],
+                        [value: 20, color: "#f1d801"],
+                        [value: 25, color: "#d04e00"],
+                        [value: 30, color: "#bc2323"],
+                        [value: 44, color: "#1e9cbb"],
+                        [value: 59, color: "#90d2a7"],
+                        [value: 74, color: "#44b621"],
+                        [value: 84, color: "#f1d801"],
+                        [value: 95, color: "#d04e00"],
+                        [value: 96, color: "#bc2323"]
+						// Celsius color set (to switch, delete the 13 lines above anmd remove the two slashes at the beginning of the line below)
+                        //[value: 0, color: "#153591"], [value: 7, color: "#1e9cbb"], [value: 15, color: "#90d2a7"], [value: 23, color: "#44b621"], [value: 28, color: "#f1d801"], [value: 35, color: "#d04e00"], [value: 37, color: "#bc2323"]
                     ]
-			}
-            
-            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
-    			attributeState("default", label:'Updated: ${currentValue}',icon: "st.Health & Wellness.health9")
+                )
             }
-		}
+            tileAttribute("device.multiAttributesReport", key: "SECONDARY_CONTROL") {
+                attributeState("multiAttributesReport", label:'${currentValue}' //icon:"st.Weather.weather12",
+                )
+            }
+        }        
+        valueTile("temperature2", "device.temperature", inactiveLabel: false) {
+            state "temperature", label:'${currentValue}', icon:"st.Weather.weather2",
+            backgroundColors:[
+                // Fahrenheit color set
+                [value: 0, color: "#153591"],
+                [value: 5, color: "#1e9cbb"],
+                [value: 10, color: "#90d2a7"],
+                [value: 15, color: "#44b621"],
+                [value: 20, color: "#f1d801"],
+                [value: 25, color: "#d04e00"],
+                [value: 30, color: "#bc2323"],
+                [value: 44, color: "#1e9cbb"],
+                [value: 59, color: "#90d2a7"],
+                [value: 74, color: "#44b621"],
+                [value: 84, color: "#f1d801"],
+                [value: 95, color: "#d04e00"],
+                [value: 96, color: "#bc2323"]
+                // Celsius color set (to switch, delete the 13 lines above anmd remove the two slashes at the beginning of the line below)
+                //[value: 0, color: "#153591"], [value: 7, color: "#1e9cbb"], [value: 15, color: "#90d2a7"], [value: 23, color: "#44b621"], [value: 28, color: "#f1d801"], [value: 35, color: "#d04e00"], [value: 37, color: "#bc2323"]
+            ]
+        }
         
-         valueTile("humidity", "device.humidity", width: 2, height: 2, unit: "%") {
+        valueTile("humidity", "device.humidity", width: 2, height: 2, unit: "%") {
             state("val", label:'${currentValue}', defaultState: true, 
             	backgroundColors:[
-                    [value: 10, color: "#bfbdef"],
-                    [value: 20, color: "#a29fea"],
-                    [value: 30, color: "#8582e0"],
-                    [value: 40, color: "#6c68e2"],
-                    [value: 50, color: "#524ed3"],
-                    [value: 60, color: "#3d39cc"],
-                    [value: 70, color: "#221dc6"],
-                    [value: 80, color: "#0d06e2"]
+                    [value: 10, color: "#153591"],
+                    [value: 30, color: "#1e9cbb"],
+                    [value: 40, color: "#90d2a7"],
+                    [value: 50, color: "#44b621"],
+                    [value: 60, color: "#f1d801"],
+                    [value: 80, color: "#d04e00"],
+                    [value: 90, color: "#bc2323"]
                 ]
             )
         }
         
         
-        valueTile("airPressure", "device.airPressure", width: 2, height: 2) {
-            state("val", label:'${currentValue}', defaultState: true, backgroundColor:"#00a0dc")
+        valueTile("pressure", "device.pressure", width: 2, height: 2, unit: "") {
+            state("val", label:'${currentValue}', defaultState: true, 
+            	backgroundColors:[
+                    [value: 10, color: "#153591"],
+                    [value: 30, color: "#1e9cbb"],
+                    [value: 40, color: "#90d2a7"],
+                    [value: 50, color: "#44b621"],
+                    [value: 60, color: "#f1d801"],
+                    [value: 80, color: "#d04e00"],
+                    [value: 90, color: "#bc2323"]
+                ]
+            )
         }
-        
-        main (["temperature"])
-      	details(["temperature", "humidity", "airPressure"])
+        valueTile("battery", "device.battery", width: 2, height: 2) {
+            state "val", label:'${currentValue}', defaultState: true
+        }		
+		
+        main("temperature2")
+        details(["temperature", "battery", "pressure", "humidity", "lastcheckin"])
+    }
+		
+		
+		
 	}
 }
 
@@ -105,63 +161,71 @@ def setStatus(params){
     log.debug "${params.key} : ${params.data}"
  
  	switch(params.key){
-    case "temperature":
-    	sendEvent(name:"temperature", value: params.data)
-    	break;
     case "relativeHumidity":
     	sendEvent(name:"humidity", value: params.data + "%")
     	break;
-    case "atmosphericPressure":
-    	sendEvent(name:"airPressure", value: params.data)
+    case "temperature":
+        sendEvent(name:"temperature", value: params.data )
     	break;
+    case "pressure":
+    	sendEvent(name:"pressure", value: params.data )
+    	break;
+    case "batteryLevel":
+    	sendEvent(name:"battery", value: params.data + "%")
+    	break;		
     }
     
     def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
     sendEvent(name: "lastCheckin", value: now)
 }
-
-def refresh(){
-	log.debug "Refresh"
-    def options = [
-     	"method": "GET",
-        "path": "/get?id=${state.id}",
-        "headers": [
-        	"HOST": state.app_url,
-            "Content-Type": "application/json"
-        ]
-    ]
-    sendCommand(options, callback)
+// If the day of month has changed from that of previous event, reset the daily min/max temp values
+def checkNewDay(now) {
+	def oldDay = ((device.currentValue("currentDay")) == null) ? "32" : (device.currentValue("currentDay"))
+	def newDay = new Date(now).format("dd")
+	if (newDay != oldDay) {
+		resetMinMax()
+		sendEvent(name: "currentDay", value: newDay, displayed: false)
+	}
 }
 
-def callback(physicalgraph.device.HubResponse hubResponse){
-	def msg
-    try {
-        msg = parseLanMessage(hubResponse.description)
-		def jsonObj = new JsonSlurper().parseText(msg.body)
-        log.debug jsonObj
-//        setStatus(jsonObj.state)
-    } catch (e) {
-        log.error "Exception caught while parsing data: "+e;
-    }
+// Reset daily min/max temp and humidity values to the current temp/humidity values
+def resetMinMax() {
+	def currentTemp = device.currentValue('temperature')
+	def currentHumidity = device.currentValue('humidity')
+    currentTemp = currentTemp ? (int) currentTemp : currentTemp
+	log.debug "${device.displayName}: Resetting daily min/max values to current temperature of ${currentTemp}° and humidity of ${currentHumidity}%"
+    sendEvent(name: "maxTemp", value: currentTemp, displayed: false)
+    sendEvent(name: "minTemp", value: currentTemp, displayed: false)
+    sendEvent(name: "maxHumidity", value: currentHumidity, displayed: false)
+    sendEvent(name: "minHumidity", value: currentHumidity, displayed: false)
+    refreshMultiAttributes()
 }
 
-def updated() {
+// Check new min or max temp for the day
+def updateMinMaxTemps(temp) {
+	temp = temp ? (int) temp : temp
+	if ((temp > device.currentValue('maxTemp')) || (device.currentValue('maxTemp') == null))
+		sendEvent(name: "maxTemp", value: temp, displayed: false)	
+	if ((temp < device.currentValue('minTemp')) || (device.currentValue('minTemp') == null))
+		sendEvent(name: "minTemp", value: temp, displayed: false)
+	refreshMultiAttributes()
 }
 
-def sendCommand(options, _callback){
-	def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: _callback])
-    sendHubCommand(myhubAction)
+// Check new min or max humidity for the day
+def updateMinMaxHumidity(humidity) {
+	if ((humidity > device.currentValue('maxHumidity')) || (device.currentValue('maxHumidity') == null))
+		sendEvent(name: "maxHumidity", value: humidity, displayed: false)
+	if ((humidity < device.currentValue('minHumidity')) || (device.currentValue('minHumidity') == null))
+		sendEvent(name: "minHumidity", value: humidity, displayed: false)
+	refreshMultiAttributes()
 }
 
-def makeCommand(body){
-	def options = [
-     	"method": "POST",
-        "path": "/control",
-        "headers": [
-        	"HOST": state.app_url,
-            "Content-Type": "application/json"
-        ],
-        "body":body
-    ]
-    return options
+// Update display of multiattributes in main tile
+def refreshMultiAttributes() {
+	def temphiloAttributes = displayTempHighLow ? (displayHumidHighLow ? "Today's High/Low:  ${device.currentState('maxTemp')?.value}° / ${device.currentState('minTemp')?.value}°" : "Today's High: ${device.currentState('maxTemp')?.value}°  /  Low: ${device.currentState('minTemp')?.value}°") : ""
+	def humidhiloAttributes = displayHumidHighLow ? (displayTempHighLow ? "    ${device.currentState('maxHumidity')?.value}% / ${device.currentState('minHumidity')?.value}%" : "Today's High: ${device.currentState('maxHumidity')?.value}%  /  Low: ${device.currentState('minHumidity')?.value}%") : ""
+	sendEvent(name: "multiAttributesReport", value: "${temphiloAttributes}${humidhiloAttributes}", displayed: false)
 }
+
+
+def updated() {}
