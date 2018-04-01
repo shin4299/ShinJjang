9bf96aa1c42747504474c7e342f9244a39f6f749
shin4299 <shin4299@gmail.com>
2018-04-01 16:04:42 +0900

    Update xiaomi-weather.groovy

--- devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
+++ devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
@@ -44,6 +44,24 @@ metadata {
 		attribute "multiAttributesReport", "String"
 		attribute "currentDay", "String"
 
+        attribute "1l", "string"
+        attribute "1t", "string"
+        attribute "1h", "string"
+        attribute "2l", "string"
+        attribute "2t", "string"
+        attribute "2h", "string"
+        attribute "3l", "string"
+        attribute "3t", "string"
+        attribute "3h", "string"
+        attribute "4l", "string"
+        attribute "4t", "string"
+        attribute "4h", "string"
+        attribute "5l", "string"
+        attribute "5t", "string"
+        attribute "5h", "string"
+        attribute "6l", "string"
+        attribute "6t", "string"
+        attribute "6h", "string"
         attribute "lastCheckin", "Date"
 		attribute "lastCheckinDate", "String"
 
@@ -82,11 +100,11 @@ metadata {
                     ]
                 )
             }
-//            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
-//    			attributeState("default", label:'Updated: ${currentValue}',icon: "st.Health & Wellness.health9")
-//            }
+            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'Updated: ${currentValue}\n')
+            }
             tileAttribute("device.multiAttributesReport", key: "SECONDARY_CONTROL") {
-                attributeState("multiAttributesReport", label:'${currentValue}' //icon:"st.Weather.weather12",
+                attributeState("multiAttributesReport", label:'\n${currentValue}' //icon:"st.Weather.weather12",
                 ) }
         }        
         valueTile("temperature2", "device.temperature", inactiveLabel: false) {
@@ -148,16 +166,74 @@ metadata {
         valueTile("humi", "title", decoration: "flat", inactiveLabel: false, width: 2, height: 1) {
 	    state "default", label:'습도'
         }		
+        valueTile("1l", "device.1l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("1t", "device.1t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("1h", "device.1h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("2l", "device.2l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("2t", "device.2t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("2h", "device.2h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("3l", "device.3l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("3t", "device.3t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("3h", "device.3h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("4l", "device.4l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("4t", "device.4t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("4h", "device.4h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("5l", "device.5l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("5t", "device.5t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("5h", "device.5h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("6l", "device.6l") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("6t", "device.6t") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+        valueTile("6h", "device.6h") {
+            state "val", label:'${currentValue}%', defaultState: true
+        }		
+//        valueTile("lastcheckin", "device.lastCheckin", inactiveLabel: false, decoration:"flat", width: 4, height: 1) {
+//        state "lastcheckin", label:'Last Event:\n ${currentValue}'
+//        }
         valueTile("bat", "title", decoration: "flat", inactiveLabel: false, width: 2, height: 1) {
 	    state "default", label:'베터리'
         }		
-        valueTile("lastcheckin", "device.lastCheckin", inactiveLabel: false, decoration:"flat", width: 4, height: 1) {
-        state "lastcheckin", label:'Last Event:\n ${currentValue}'
-        }
 
 
         main("temperature2")
-        details(["temperature", "humi", "pre", "bat", "humidity", "pressure", "battery", "lastcheckin"])
+        details(["temperature", "humi", "pre", "bat", "humidity", "pressure", "battery",
+		"1l", "2l", "3l", "4l", "5l", "6l", 
+		"1t", "2t", "3t", "4t", "5t", "6t", 
+		"1h", "2h", "3h", "4h", "5h", "6h", 
+		])
     }
 }
 
@@ -229,6 +305,48 @@ def checkNewDay() {
 }
 
 def resetMinMax() {
+	def day = new Date().format("EEE", location.timeZone)	
+	def currentMaxTemp = device.currentValue('maxTemp')
+	def currentMinTemp = device.currentValue('mimTemp')
+	def currentMaxHumi = device.currentValue('maxHumidity')
+	def currentMinHumi = device.currentValue('minHumidity')	
+	if (day == "Mon") {
+		 state.sunMaxTemp = currentMaxTemp
+		 state.sunMinTemp = currentMinTemp
+		 state.sunMaxHumi = currentMaxHumi
+		 state.sunMinHumi = currentMinHumi
+	} else if (day == "Tue") {
+		 state.monMaxTemp = currentMaxTemp
+		 state.monMinTemp = currentMinTemp
+		 state.monMaxHumi = currentMaxHumi
+		 state.monMinHumi = currentMinHumi
+	} else if (day == "Wed") {
+		 state.tueMaxTemp = currentMaxTemp
+		 state.tueMinTemp = currentMinTemp
+		 state.tueMaxHumi = currentMaxHumi
+		 state.tueMinHumi = currentMinHumi
+	} else if (day == "Thu") {
+		 state.wedMaxTemp = currentMaxTemp
+		 state.wedMinTemp = currentMinTemp
+		 state.wedMaxHumi = currentMaxHumi
+		 state.wedMinHumi = currentMinHumi
+	} else if (day == "Fri") {
+		 state.thuMaxTemp = currentMaxTemp
+		 state.thuMinTemp = currentMinTemp
+		 state.thuMaxHumi = currentMaxHumi
+		 state.thuMinHumi = currentMinHumi
+	} else if (day == "Sat") {
+		 state.firMaxTemp = currentMaxTemp
+		 state.firMinTemp = currentMinTemp
+		 state.firMaxHumi = currentMaxHumi
+		 state.firMinHumi = currentMinHumi
+	} else if (day == "Sun") {
+		 state.satMaxTemp = currentMaxTemp
+		 state.satMinTemp = currentMinTemp
+		 state.satMaxHumi = currentMaxHumi
+		 state.satMinHumi = currentMinHumi
+	}
+	
 	def currentTemp = device.currentValue('temperature')
 	def currentHumidity = device.currentValue('humidity')
     currentTemp = currentTemp ? (int) currentTemp : currentTemp
@@ -264,5 +382,140 @@ def refreshMultiAttributes() {
 	def temphiloAttributes = displayTempHighLow ? (displayHumidHighLow ? "Today's High/Low:  ${device.currentState('maxTemp')?.value}° / ${device.currentState('minTemp')?.value}°" : "Today's High: ${device.currentState('maxTemp')?.value}°  /  Low: ${device.currentState('minTemp')?.value}°") : ""
 	def humidhiloAttributes = displayHumidHighLow ? (displayTempHighLow ? "    ${device.currentState('maxHumidity')?.value}% / ${device.currentState('minHumidity')?.value}%" : "Today's High: ${device.currentState('maxHumidity')?.value}%  /  Low: ${device.currentState('minHumidity')?.value}%") : ""
 	sendEvent(name: "multiAttributesReport", value: "${temphiloAttributes}${humidhiloAttributes}", displayed: false)
+	if (day == "Mon") {
+		sendEvent(name: "1l", value: "TUE")
+		sendEvent(name: "2l", value: "WED")
+		sendEvent(name: "3l", value: "THU")
+		sendEvent(name: "4l", value: "FRI")
+		sendEvent(name: "5l", value: "SAT")
+		sendEvent(name: "6l", value: "SUN")
+		sendEvent(name: "1t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "2t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "3t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "4t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "5t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "6t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "1h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "2h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "3h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "4h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "5h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "6h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Tue") {
+		sendEvent(name: "6l", value: "MON")
+		sendEvent(name: "1l", value: "WED")
+		sendEvent(name: "2l", value: "THU")
+		sendEvent(name: "3l", value: "FRI")
+		sendEvent(name: "4l", value: "SAT")
+		sendEvent(name: "5l", value: "SUN")
+		sendEvent(name: "6t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "1t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "2t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "3t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "4t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "5t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "6h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "1h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "2h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "3h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "4h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "5h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Wed") {
+		sendEvent(name: "6l", value: "TUE")
+		sendEvent(name: "5l", value: "MON")
+		sendEvent(name: "l", value: "THU")
+		sendEvent(name: "2l", value: "FRI")
+		sendEvent(name: "3l", value: "SAT")
+		sendEvent(name: "4l", value: "SUN")
+		sendEvent(name: "6t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "5t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "1t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "2t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "3t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "4t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "6h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "5h", value: state.monMaxHumi + "\n" + state.monMinHumi)
+		sendEvent(name: "1h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "2h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "3h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "4h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Thu") {
+		sendEvent(name: "5l", value: "TUE")
+		sendEvent(name: "6l", value: "WED")
+		sendEvent(name: "4l", value: "MON")
+		sendEvent(name: "1l", value: "FRI")
+		sendEvent(name: "2l", value: "SAT")
+		sendEvent(name: "3l", value: "SUN")
+		sendEvent(name: "5t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "6t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "4t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "1t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "2t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "3t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "5h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "6h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "4h", value: state.monMaxHumi + "\n" + state.monMinHumi)
+		sendEvent(name: "1h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "2h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "3h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Fri") {
+		sendEvent(name: "4l", value: "TUE")
+		sendEvent(name: "5l", value: "WED")
+		sendEvent(name: "6l", value: "THU")
+		sendEvent(name: "3l", value: "MON")
+		sendEvent(name: "1l", value: "SAT")
+		sendEvent(name: "2l", value: "SUN")
+		sendEvent(name: "4t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "5t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "6t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "3t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "1t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "2t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "4h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "5h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "6h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "3h", value: state.monMaxHumi + "\n" + state.monMinHumi)
+		sendEvent(name: "1h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "2h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Sat") {
+		sendEvent(name: "3l", value: "TUE")
+		sendEvent(name: "4l", value: "WED")
+		sendEvent(name: "5l", value: "THU")
+		sendEvent(name: "6l", value: "FRI")
+		sendEvent(name: "2l", value: "MON")
+		sendEvent(name: "1l", value: "SUN")
+		sendEvent(name: "3t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "4t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "5t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "6t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "2t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "1t", value: state.sunMaxTemp + "\n" + state.sunMinTemp)
+		sendEvent(name: "3h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "4h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "5h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "6h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "2h", value: state.monMaxHumi + "\n" + state.monMinHumi)
+		sendEvent(name: "1h", value: state.sunMaxHumi + "\n" + state.sunMinHumi)
+	} else if (day == "Sun") {
+		sendEvent(name: "2l", value: "TUE")
+		sendEvent(name: "3l", value: "WED")
+		sendEvent(name: "4l", value: "THU")
+		sendEvent(name: "5l", value: "FRI")
+		sendEvent(name: "6l", value: "SAT")
+		sendEvent(name: "1l", value: "MON")
+		sendEvent(name: "2t", value: state.tueMaxTemp + "\n" + state.tueMinTemp)
+		sendEvent(name: "3t", value: state.wedMaxTemp + "\n" + state.wedMinTemp)
+		sendEvent(name: "4t", value: state.thuMaxTemp + "\n" + state.thuMinTemp)
+		sendEvent(name: "5t", value: state.friMaxTemp + "\n" + state.friMinTemp)
+		sendEvent(name: "6t", value: state.satMaxTemp + "\n" + state.satMinTemp)
+		sendEvent(name: "1t", value: state.monMaxTemp + "\n" + state.monMinTemp)
+		sendEvent(name: "2h", value: state.tueMaxHumi + "\n" + state.tueMinHumi)
+		sendEvent(name: "3h", value: state.wedMaxHumi + "\n" + state.wedMinHumi)
+		sendEvent(name: "4h", value: state.thuMaxHumi + "\n" + state.thuMinHumi)
+		sendEvent(name: "5h", value: state.friMaxHumi + "\n" + state.friMinHumi)
+		sendEvent(name: "6h", value: state.satMaxHumi + "\n" + state.satMinHumi)
+		sendEvent(name: "1h", value: state.monMaxHumi + "\n" + state.monMinHumi)
+	} 
+		
 }
 
