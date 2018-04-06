a75b879a4e1b4e18c1e9a19a7392b4bb27393ac0
Jangick Shin <shin4299@icloud.com>
2018-04-06 12:01:30 +0900

    http

--- devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
+++ devicetypes/fison67/xiaomi-weather.src/xiaomi-weather.groovy
@@ -314,36 +314,97 @@ mappings {
 }
 
 def main() {
+def sign = '$'
     renderHTML() {
         head {
             """
-                <meta name="viewport" content="initial-scale=1.0">
-                <meta charset="utf-8">
-                <style>
-                    body {
-                        font-size: 1em;
-                        color: #888888;
-                    }
-                    a {
-                        color: #15bfff;
-                    }
-                </style>
+  
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>  
+<script src="http://code.highcharts.com/highcharts.js"></script>  
+  
+
             """
         }
         body {
-            """
-            <div id="home" style="text-align: center; padding-top:15px;">
-            This device is known to be incompatible with SmartThings and may not function as expected or cause other devices to malfunction.</br>
-            For more information go to:</br></br>
-	    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/467919/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
-            </div>
+            """            
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/series-label.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+<script>  
+
+Highcharts.chart('container', {
+
+    title: {
+        text: 'Solar Employment Growth by Sector, 2010-2016'
+    },
+
+    subtitle: {
+        text: 'Source: thesolarfoundation.com'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});</script>  
+
             """
         }
     }
 }
 
 def whitelist() {
-    ["https://thingspeak.com"]
+    ["code.highcharts.com",
+    "ajax.googleapis.com"]
 }
 //--------------------------------------
 
